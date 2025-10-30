32726d4d8034219c17c22f844eb9f2c0c88dd5fa
Anish Kumar <29kumaranish@gmail.com>
2025-10-30 18:43:56 +0530

    Implemented the add function

--- tree.c
+++ tree.c
@@ -24,17 +24,64 @@ BST * create_node()
 bool add(BST *root)
 {
     BST *temp=create_node();
-    char 
-    printf("Enter the Name:");
-    
+    printf("Enter Your  Name:");
+    scanf("%s",&temp->name);
+    printf("Enter Your Age:");
+    scanf("%d",&temp->age);
+    printf("Enter Your ID:");
+    scanf("%d",&temp->Id);
+    printf("Enter your Phone Number:");
+    scanf("%d",&temp->phone_number);
+    printf("Enter Your Email ID:");
+    scanf("%s",&temp->email);
+    BST *loop=root;
+    BST *parent = NULL;
 
-    if(root==NULL)
+    while(loop != NULL)
     {
-        
+        parent = loop;
+        if(temp->Id < loop->Id)
+        {
+            loop = loop->left;
+        }
+        else if(temp->Id > loop->Id)
+        {
+            loop = loop->right;
+        }
+        else if(temp->Id == loop->Id)
+        {
+            free(temp);
+            printf("The Value of ID already exists!");
+            return false;
+        }
     }
+
+    if(temp->Id < parent->Id)
+    {
+        parent->left = temp;
+    }
+    else
+    {
+        parent->right = temp;
+    }
+    
+    return true;
+};
+void inorder(BST *root)
+{
+    BST* temp=root;
+    inorder(temp->left);
+    printf("Name is %s\n",temp->name);
+    printf("Age is %d\n",temp->age);
+    printf("Id is %d",temp->Id);
+    printf("Phone Number is %d",temp->phone_number);
+    printf("Email is %d",temp->email);
+    inorder(temp->right);
+
 };
 
-void inorder(BST *root);
-
-bool remove (BST *root);
+bool remove (BST *root)
+{
+    
+};
 
